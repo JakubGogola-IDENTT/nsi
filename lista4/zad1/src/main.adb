52048fd4204213e4a8da20a54777e58655c9542a
JakubGogola <jakubgogola97@gmail.com>
2019-05-11 13:05:26 +0200

    l4: some examples

--- lista4/zad1/src/main.adb
+++ lista4/zad1/src/main.adb
@@ -2,8 +2,42 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Railway; use Railway;
 
 procedure Main is
-
+   Success : Boolean;
 begin
-   --  Insert code here.
-   null;
+   Put_Line ("#################################");
+   Put_Line ("[---->||     |     |     ||     ]");
+   Open_Route (Route_Enter_Left, Success);
+   Put_Line ("Open route result: " & Success'Image);
+   Move_Train (Route_Enter_Left, Success);
+   Put_Line ("Move train result: " & Success'Image);
+   Put_Line ("[     ||---->|     |     ||     ]");
+   Put_Line ("#################################");
+
+   Put_Line ("#################################");
+   Put_Line ("[     ||---->|     |     ||     ]");
+   Open_Route (Route_Left_Middle, Success);
+   Put_Line ("Open route result: " & Success'Image);
+   Move_Train (Route_Left_Middle, Success);
+   Put_Line ("Move train result: " & Success'Image);
+   Put_Line ("[     ||     |---->|     ||     ]");
+   Put_Line ("#################################");
+
+   Put_Line ("#################################");
+   Put_Line ("[     ||     |---->|     ||     ]");
+   Open_Route (Route_Middle_Right, Success);
+   Put_Line ("Open route result: " & Success'Image);
+   Move_Train (Route_Middle_Right, Success);
+   Put_Line ("Move train result: " & Success'Image);
+   Put_Line ("[     ||     |     |---->||     ]");
+   Put_Line ("#################################");
+
+   Put_Line ("#################################");
+   Put_Line ("[     ||     |     |---->||     ]");
+   Open_Route (Route_Leave_Right, Success);
+   Put_Line ("Open route result: " & Success'Image);
+   Move_Train (Route_Leave_Right, Success);
+   Put_Line ("Move train result: " & Success'Image);
+   Put_Line ("[     ||     |     |     ||---->]");
+   Put_Line ("#################################");
+
 end Main;
